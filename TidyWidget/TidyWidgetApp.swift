8eb9dc2de2c8c4175741b2652cd669a56c090046
yosign <crpboy@gmail.com>
2024-12-25 14:13:52 +0800

    Initial Commit

---
+++ TidyWidget/TidyWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TidyWidgetApp.swift
+//  TidyWidget
+//
+//  Created by YoSign on 2024/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TidyWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TidyWidgetTests/TidyWidgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  TidyWidgetTests.swift
+//  TidyWidgetTests
+//
+//  Created by YoSign on 2024/12/25.
+//
+
+import Testing
+@testable import TidyWidget
+
+struct TidyWidgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
